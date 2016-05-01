4f1ca93e8396cebdab7de4dc13790ae8af46016e
Mike <duredhil@gmail.com>
2016-04-30 22:01:16 -0400

    Working through Chapter 4.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require("../controllers/main");
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-var homepageController = function (req, res) {
-	res.render("index", {title: "Express"});
-};
+/* Locations Pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
 
-/* GET home page */
-router.get("/", ctrlMain.index);
+/* Other Pages */
+
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
